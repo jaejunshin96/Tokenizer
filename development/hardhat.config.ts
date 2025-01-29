1b795004f0945500778225935dafb4e9c67be0f7
Jaejun Shin <jaeshinkorean@gmail.com>
2025-01-29 20:04:02 +1030

    migrate to hardhat vars from dotenv

--- development/hardhat.config.ts
+++ development/hardhat.config.ts
@@ -1,20 +1,21 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
-import * as dotenv from "dotenv";
 
-const privateKey = process.env.PRIVATE_KEY || "";
+const { vars } = require("hardhat/config");
+
+const privateKey = vars.get("PRIVATE_KEY", "");
 
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
-//  defaultNetwork: "sepolia",
-//  networks: {
-//    hardhat: {
-//    },
-//    sepolia: {
-//      url: "https://sepolia.infura.io/v3/<key>",
-//      accounts: [privateKey]
-//    }
-//  }
+  defaultNetwork: "sepolia",
+  networks: {
+    hardhat: {
+    },
+    sepolia: {
+      url: "https://sepolia.infura.io/v3/<key>",
+      accounts: [privateKey]
+    }
+  }
 };
 
 export default config;
