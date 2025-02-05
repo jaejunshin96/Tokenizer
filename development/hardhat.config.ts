72ec7671630509a5d518c6a09c61714a2e593c53
Jaejun Shin <jaeshinkorean@gmail.com>
2025-02-05 22:14:26 +1030

    add etherscan api key

--- development/hardhat.config.ts
+++ development/hardhat.config.ts
@@ -5,6 +5,7 @@ const { vars } = require("hardhat/config");
 
 const PRIVATE_KEY = vars.get("PRIVATE_KEY", "");
 const INFURA_API_KEY = vars.get("INFURA_API_KEY", "");
+const ETHERSCAN_API_KEY = vars.get("ETHERSCAN_API_KEY", "");
 
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
@@ -15,8 +16,13 @@ const config: HardhatUserConfig = {
     sepolia: {
       url: `https://sepolia.infura.io/v3/${INFURA_API_KEY}`,
       accounts: [PRIVATE_KEY]
-    }
-  }
+    },
+  },
+  etherscan: {
+    apiKey: {
+      sepolia: ETHERSCAN_API_KEY,
+    },
+  },
 };
 
 export default config;
